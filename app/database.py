b7b70aadbe0d5f0ab011b5934715e48944f25783
aabhirup <aabhirupsamanta20@gmail.com>
2025-12-28 17:51:08 +0530

    Fix typo in database.py imports

--- app/database.py
+++ app/database.py
@@ -1,5 +1,6 @@
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_enginer
+from sqlalchemy.orm import sessionmaker
 import os
 
 # Using sqlite for default if POSTGRES_URL not provided for easier local running, 
